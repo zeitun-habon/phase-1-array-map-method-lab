d6af7602988551dfb4ed0e2046cd020d546a5c5c
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-11 15:34:19 +0300

    commit tree 1790173e8ec32728660f64ca68a30cc1f26c469e

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((textSentence) => {
+    const splitSentence = textSentence.split(" ");
+    const splitSentenceToUppercase = splitSentence.map((splitSentence) => splitSentence.charAt(0).toUpperCase() +splitSentence.slice(1));
+    const result = splitSentenceToUppercase.join(" ");
+    return result;
+  });
+  
+};
